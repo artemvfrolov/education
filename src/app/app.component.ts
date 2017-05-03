7c8206cf1d982543219115ac8e3eb1c0807045ca
artem <artem.frolov@valor-softvare.com>
2017-05-03 15:07:08 +0300

    Create App

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,73 @@
 import { Component } from '@angular/core';
+import { TodoStore, Todo } from './service/store.service';
 
 @Component({
-  selector: 'app-root',
+  selector: 'todo-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'app works!';
+
+export class TodoApp {
+  todoStore: TodoStore;
+  newTodoText = '';
+  todosClone: Array<Todo>;
+
+  constructor(todoStore: TodoStore) {
+    this.todoStore = todoStore;
+    this.todosClone = todoStore.todos;
+  }
+
+  removeCompleted() {
+    this.todoStore.removeCompleted();
+  }
+
+  toggleCompletion(todo: Todo) {
+    this.todoStore.toggleCompletion(todo);
+  }
+
+  remove(todo: Todo){
+    this.todoStore.remove(todo);
+  }
+
+  addTodo(form) {
+    event.preventDefault();
+
+    this.newTodoText = form.value.todo;
+
+    if (this.newTodoText.trim().length) {
+      this.todoStore.add(this.newTodoText);
+      this.newTodoText = '';
+      form.reset();
+    }
+  }
+
+  filter(type: string) {
+    event.preventDefault();
+
+    let arrAll = this.todoStore.todos;
+    let arrActive = [];
+    let arrCompleted = [];
+
+    arrAll.forEach(function (item) {
+      if (item.completed){
+        arrActive.push(item);
+      }else{
+        arrCompleted.push(item);
+      }
+    });
+
+    switch (type){
+      case  'all':
+        this.todosClone = arrAll;
+        break;
+
+      case  'active':
+        this.todosClone = arrCompleted;
+        break;
+
+      case  'completed':
+        this.todosClone = arrActive;
+        break;
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
-import { AppComponent } from './app.component';
+import { TodoApp } from './app.component';
+
+import { TodoStore } from './service/store.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    TodoApp
   ],
   imports: [
     BrowserModule,
     FormsModule,
+    ReactiveFormsModule,
     HttpModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [TodoStore],
+  bootstrap: [TodoApp]
 })
 export class AppModule { }
